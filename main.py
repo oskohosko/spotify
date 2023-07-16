4c70fd839e12fa331b35714c56a314431fc102ff
Oskar Hosken <oskarhosken@gmail.com>
2023-07-16 15:17:26 +1000

    Added genres to main dataframe

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ load_dotenv()
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
+print("Initialising request...\n")
+
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
@@ -21,16 +23,25 @@ TECHNO = "spotify:Oskar_Hosken:TECHNO:playlist:1lGXPfqwCeSqZ0O5h4yps0"
 
 PROVINCIAL = "spotify:Oskar_Hosken:PROVINCIAL:playlist:07o6kTp4nYhgcbIvVQpcEq"
 
+TEST_ALBUM = "spotify:album:3fSff3bKd7pS7kLYiNakMV"
+
+NPH = "500YRyClzP6Z7HtWd1BIje"
+
 # To test before making a json file with all the info etc
 playlist_uri = "spotify:Oskar_Hosken:PROVINCIAL:playlist:07o6kTp4nYhgcbIvVQpcEq"
 like = True
 
 username, playlist_id, playlist_name = playlist_uri.split(':')[1], playlist_uri.split(':')[4], playlist_uri.split(':')[2]
 # Returning a dict of all the tracks
+
+print("Retrieving playlist...\n")
+
 sp_tracks = sp.user_playlist(username, playlist_id, 'tracks')
 
 # Creating a dataframe with basic details on the track, e.g artist name, album name, track name
 playlist_df = pd.DataFrame(columns=sp_tracks['tracks']['items'][0]['track'].keys())
+
+print("Populating playlist DataFrame...\n")
 
 # Adding the data into the df
 for track in sp_tracks['tracks']['items']:
@@ -38,8 +49,9 @@ for track in sp_tracks['tracks']['items']:
 
 # Cleaning up columns in our dataframe to make them more readable and to consist of only relevant/interesting data.
 
-# First removing any columns that are completely of no interest
+print("Cleaning DataFrame...\n")
 
+# First removing any columns that are completely of no interest
 playlist_df = playlist_df.drop(columns=['available_markets', 'disc_number', 'episode', 'explicit', 'external_ids', 'external_urls', 'href', 'is_local', 'track', 'track_number', 'type', 'uri', 'preview_url'])
 
 # Getting the artist's id from the album
@@ -67,8 +79,11 @@ playlist_df['album'] = playlist_df['album'].apply(lambda x: filter_album(x))
 # We just want the artist's name from that column
 playlist_df['artists'] = playlist_df['artists'].apply(lambda x: x[0]['name'])
 
-# Putting the data into a csv file
-playlist_df.to_csv('results.csv')
+print("Playlist DF Complete!\n")
+
+
+
+print("Analysing artists...\n")
 
 """
 My code to get the album info. It was not helpful for what I wanted to achieve so I've commented it out.
@@ -85,5 +100,16 @@ for album in playlist_df['album']:
 album_df.to_csv('albums.csv')
 
 """
-
 # Okay so after some trial and error I'm not able to get any genres from any of the albums. We're going to try the artists now.
+artist_df = pd.DataFrame(columns=sp.artist(NPH).keys())
+
+for artist in playlist_df['artist_id']:
+    artist_df.loc[len(artist_df)] = sp.artist(artist).values()
+
+# artist_df.to_csv('artists.csv')
+
+playlist_df['genres'] = artist_df['genres']
+
+# Putting the data into a csv file
+playlist_df.to_csv('results.csv')
+print("Complete.")
